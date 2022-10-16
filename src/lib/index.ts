79e126b42eb69611d2d34fcb2904af9010d97f29
JeanJPNM <jeanjpnm1@gmail.com>
2022-10-16 13:52:49 -0300

    export types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
 export { default as Editor } from './Editor.svelte';
+export type { EditorChangeEvent, EditorMountEvent, EditorValidateEvent } from './Editor.svelte';
 export { default as DiffEditor } from './DiffEditor.svelte';
+export type { DiffEditorChangeEvent, DiffEditorMountEvent } from './DiffEditor.svelte';
+export type { Monaco } from './types';
